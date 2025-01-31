a41aadabc17d79bb829e264be4a0f50f15b19089
Tristine <tristineferriera@gmail.com>
2025-01-30 16:22:51 -0800

    Lab 3

---
+++ DAGV1400/Assets/Scripts/3/ApplyForce.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyForce : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.right * 500);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Collision detected with" + other.gameObject.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
