4970e7cd0349dbcc5210b9662c9d9f54e3567c50
Tristine <tristineferriera@gmail.com>
2025-02-05 19:50:45 -0800

    Lab 4

---
+++ DAGV1400/Assets/Scripts/4/SimpleCharacterController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCharacterController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private CharacterController controller;
+    private Transform thisTransform;
+    private Vector3 movementVector = Vector3.zero;
+
+    private void MoveCharacter()
+    {
+        movementVector.x = Input.GetAxis("Horizontal");
+        movementVector *= (moveSpeed * Time.deltaTime);
+        controller.Move(movementVector);
+    }
+
+    private void KeepCharacterOnXAxis()
+    {
+        var currentPosition = thisTransform.position;
+        currentPosition.z = 0f;
+        thisTransform.position = currentPosition;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        thisTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCharacter();
+        KeepCharacterOnXAxis();
+    }
+    
+}
