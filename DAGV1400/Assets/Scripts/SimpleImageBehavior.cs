9e447a6b02badb55d251f0d693834436d9af1ea3
Tristine <tristineferriera@gmail.com>
2025-02-28 16:26:04 -0800

    Challenge 7

--- DAGV1400/Assets/Scripts/SimpleImageBehavior.cs
+++ DAGV1400/Assets/Scripts/SimpleImageBehavior.cs
@@ -13,11 +13,38 @@ public class SimpleImageBehavior : MonoBehaviour
     void Start()
     {
         imageObj = GetComponent<Image>();
+        if (imageObj == null)
+        {
+            Debug.LogError("No Image component found on " + gameObject.name);
+        }
+        else
+        {
+            Debug.Log(gameObject.name + " found Image: " + imageObj);
+        }
     }
 
     public void UpdateWithFloatData()
     {
-        imageObj.fillAmount = dataObj.value;
-    }
+        
+        Debug.Log("UpdateWithFloatData() called on: " + gameObject.name);
     
+        if (imageObj == null)
+        {
+            Debug.LogError(gameObject.name + " imageObj is NULL when calling UpdateWithFloatData()!");
+            return;
+        }
+
+        if (dataObj == null)
+        {
+            Debug.LogError(gameObject.name + " dataObj is NULL!");
+            return;
+        }
+
+        Debug.Log(gameObject.name + " updating fillAmount to " + dataObj.value);
+        imageObj.fillAmount = dataObj.value;
+        
+        
+
+    }
+     
 }
