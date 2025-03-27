a25d4c5a60e47cddd35a48aa05061400e862c22a
Tristine <tristineferriera@gmail.com>
2025-03-26 20:12:22 -0700

    Challenge 10

--- DAGV1400/Assets/Scripts/SimpleParticleTrigger.cs
+++ DAGV1400/Assets/Scripts/SimpleParticleTrigger.cs
@@ -7,20 +7,50 @@ public class SimpleParticleTrigger : MonoBehaviour
 {
     
     private ParticleSystem particleSystem;
-    public int particleAmount = 10;
+    
+    public int firstParticleAmount = 10;
+    public int secondParticleAmount = 20;
+    public int thirdParticleAmount = 30;
+    public float delayBetweenEmissions = 0.5f;
     
     // Start is called before the first frame update
     void Start()
     {
         particleSystem = GetComponent<ParticleSystem>();
     }
+    
+    
+/*   this is the lab's part
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            particleSystem.Emit(firstParticleAmount);
+        }
+        
+    }
+*/
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            particleSystem.Emit(particleAmount);
+            StartCoroutine(EmitParticlesCoroutine());
         }
+    }
+    
+    private IEnumerator EmitParticlesCoroutine()
+    {
+        particleSystem.Emit(firstParticleAmount);
+        yield return new WaitForSeconds(delayBetweenEmissions);
+       
+        particleSystem.Emit(secondParticleAmount);
+        yield return new WaitForSeconds(delayBetweenEmissions);
+        
+        particleSystem.Emit(thirdParticleAmount);
         
     }
+    
+    
+    
 }
